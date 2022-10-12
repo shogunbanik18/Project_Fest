e8759f42493dc63a0c96bed1aac17b15bcf85770
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-10-12 16:16:11 +0530

    Update simple_calculator.py

--- Hacktoberfest2022/simple_calculator.py
+++ Hacktoberfest2022/simple_calculator.py
@@ -4,18 +4,6 @@
 def add(x, y):
     return x + y
 
-# This function subtracts two numbers
-def subtract(x, y):
-    return x - y
-
-# This function multiplies two numbers
-def multiply(x, y):
-    return x * y
-
-# This function divides two numbers
-def divide(x, y):
-    return x / y
-
 
 print("Select operation.")
 print("1.Add")
